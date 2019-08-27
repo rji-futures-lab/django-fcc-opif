df8d298aa751965c33a28576e7100c303d413dbb
James Gordon <gordon.je@gmail.com>
2019-08-27 13:42:17 -0500

    set up django project

---
+++ config/settings/base.py
@@ -0,0 +1,104 @@
+"""Django settings when running project in any environment."""
+import os
+import configparser
+
+DEBUG = False
+
+ROOT_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+django_env = os.getenv('DJANGO_ENV', 'local')
+
+config = configparser.RawConfigParser()
+config.read(os.path.join(ROOT_DIR, 'secrets.cfg'))
+secrets = config[django_env]
+
+SECRET_KEY = secrets.get('django_secret_key')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'fcc_opif',
+]
+
+SITE_ID = 1
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': secrets.get('db_name'),
+        'USER': secrets.get('db_user'),
+        'PASSWORD': secrets.get('db_password', ''),
+        'HOST': secrets.get('db_host'),
+        'PORT': secrets.getint('db_port'),
+    }
+}
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa
+    },
+]
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Chicago'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LOGIN_URL = '/admin/login/'
---
+++ fcc_opif/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FCCOPIFConfig(AppConfig):
+    name = 'fcc_opif'
