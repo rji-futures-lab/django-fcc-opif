74b54de636d20b9cc3ef6f171329a9b6a74b2944
Jacob LaGesse <jacoblagesse@gmail.com>
2019-09-26 12:51:13 -0500

    Cable System stuff

--- fcc_opif/admin.py
+++ fcc_opif/admin.py
@@ -1,7 +1,7 @@
 import csv
 from django.contrib import admin
 from django.http import HttpResponse
-from fcc_opif.models import Facility, FacilityFolder, FacilityFile
+from fcc_opif.models import Facility, FacilityFolder, FacilityFile, CableSystem, CableCommunity, CableFolder, CableFile
 
 
 class ExportCsvMixin:
@@ -30,17 +30,10 @@ class InputFilter(admin.SimpleListFilter):
     template = 'admin/input_filter.html'
 
     def lookups(self, request, model_admin):
-        # Dummy, required to show the filter.
         return ((),)
 
     def choices(self, changelist):
-        # Grab only the "all" option.
         all_choice = next(super().choices(changelist))
-        '''all_choice['query_parts'] = (
-            (k, v)
-            for k, v in changelist.get_filters_params().items()
-            if k != self.pyparameter_name
-        )'''
         yield all_choice
 
 
@@ -92,7 +85,7 @@ class FolderFacilityFilter(InputFilter):
             )
 
 
-class FolderParentFilter(InputFilter):
+class FacilityFolderParentFilter(InputFilter):
     parameter_name = 'parent_folder'
     title = ('Parent Folder')
 
@@ -103,25 +96,16 @@ class FolderParentFilter(InputFilter):
                 parent_folder__folder_path=uid
             )
 
-
-'''
-class GenericFilter(InputFilter):
-
-    def __init__(self, parameter_name, title):
-        self.parameter_name = parameter_name
-        self.title = title
-
-    parameter_name = 'l'
-    title = 'l'
+class CommunityCableSystemFilter(InputFilter):
+    parameter_name = 'system'
+    title = ('Cable System')
 
     def queryset(self, request, queryset):
         if self.value() is not None:
             uid = self.value()
             return queryset.filter(
-                Q(uid)
+                system__legal_name=uid
             )
-'''
-
 
 class FacilityAdmin(admin.ModelAdmin):
 
@@ -170,10 +154,9 @@ class FacilityAdmin(admin.ModelAdmin):
 
 admin.site.register(Facility, FacilityAdmin)
 
-
 class FolderAdmin(admin.ModelAdmin):
 
-    list_filter = ('entity__call_sign', FolderParentFilter)
+    #list_filter = ('entity__call_sign', FacilityFolderParentFilter)
     search_fields = ['folder_path', 'entity_folder_id']
     readonly_fields = (
         'entity_folder_id',
@@ -201,7 +184,7 @@ class FolderAdmin(admin.ModelAdmin):
 
 
 admin.site.register(FacilityFolder, FolderAdmin)
-
+admin.site.register(CableFolder, FolderAdmin)
 
 class FileAdmin(admin.ModelAdmin, ExportCsvMixin):
 
@@ -249,5 +232,87 @@ class FileAdmin(admin.ModelAdmin, ExportCsvMixin):
     def has_delete_permission(self, request, obj=None):
         return False
 
-
 admin.site.register(FacilityFile, FileAdmin)
+admin.site.register(CableFile, FileAdmin)
+
+class CableSystemAdmin(admin.ModelAdmin):
+
+    search_fields = ('legal_name',)
+    readonly_fields = (
+        "id",
+        "legal_name",
+        "service_type",
+        "operator_address_line1",
+        "operator_name",
+        "operator_address_line2",
+        "operator_po_box",
+        "operator_city",
+        "operator_zipcode",
+        "operator_zipcode_suffix",
+        "operator_state",
+        "operator_email",
+        "operator_website",
+        "operator_phone",
+        "operator_fax",
+        "cores_user",
+        "principal_headend_name",
+        "principal_address_line1",
+        "principal_address_line2",
+        "principal_po_box",
+        "principal_city",
+        "principal_state",
+        "principal_zipcode",
+        "principal_zipcode_suffix",
+        "principal_fax",
+        "principal_phone",
+        "principal_email",
+        "local_file_contact_name",
+        "local_file_address_line1",
+        "local_file_address_line2",
+        "local_file_po_box",
+        "local_file_city",
+        "local_file_state",
+        "local_file_zipcode",
+        "local_file_zipcode_suffix",
+        "local_file_contact_fax",
+        "local_file_contact_phone",
+        "local_file_contact_email",
+        "active_ind",
+        "principal_address_in_local_files",
+        "cable_service_zip_codes",
+        "cable_service_emp_units",
+        "cable_communities",
+    )
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(CableSystem, CableSystemAdmin)
+
+class CableCommunityAdmin(admin.ModelAdmin):
+
+    list_filter = (CommunityCableSystemFilter, 'system__legal_name')
+    search_fields = ['community_name']
+    readonly_fields = (
+        "community_unit_id",
+        "system",
+        "community_name",
+        "county_name",
+    )
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(CableCommunity, CableCommunityAdmin)
