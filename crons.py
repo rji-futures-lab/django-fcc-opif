1dd891a8bd616647f86732a14159d1e166f8b1d5
James Gordon <gordon.je@gmail.com>
2019-11-22 14:16:40 -0600

    stagger updates

--- crons.py
+++ crons.py
@@ -1,6 +1,8 @@
-from zappa.asynchronous import task
 from django.core.management import call_command
 from django.core.management.base import CommandError
+from django.db.models import Q
+from django.utils import timezone
+from zappa.asynchronous import task
 from fcc_opif.models import Facility, CableSystem
 import logging
 
@@ -16,6 +18,7 @@ def update_facility(facility_id):
     except CommandError as e:
         logger.error(e)
 
+
 @task()
 def update_cable_system(id):
     try:
@@ -26,11 +29,30 @@ def update_cable_system(id):
         logger.error(e)
 
 
-def main():
-    for facility in Facility.objects.all():
-        update_facility(facility.id)
-    logger.info(f'Initialized update for all facilities.')
+def handle_facilities():
+    call_command('getfacilities')
 
-    for cable_system in CableSystem.objects.all():
-        update_cable_system(cable_system.id)
-    logger.info(f'Initialized update for all cable systems.')
+    yesterday = timezone.now() - timezone.timedelta(days=1)
+
+    facilities = Facility.objects.filter(
+        Q(last_refreshed_ts__isnull=True) | Q(last_refreshed_ts__gt=yesterday)
+    )[:100]
+
+    for facility in facilities:
+        update_facility(facility.id)
+    logger.info(f'Initialized update for {len(facilities)} facilities.')
+
+
+def handle_cable_systems():
+    # call_command('getcablesystems')
+
+    # for cable_system in CableSystem.objects.all():
+    #     update_cable_system(cable_system.id)
+    # logger.info(f'Initialized update for all cable systems.')
+
+
+def main():
+
+    handle_facilities()
+
+    # handle_cable_systems()
